08c0bc3c310576763279d2bb7b45a5e22f5a80a4
YouFunny <32274837+youfunny@users.noreply.github.com>
2021-07-12 03:35:27 +0300

    Delete src directory

--- src/main/java/com/example/studentslife/ItemActive.kt
+++
@@ -1,3 +0,0 @@
-package com.example.studentslife
-
-data class ItemActive(val nameItem: String, val upgrade: Int, val id: String, var cost: Int)
--- src/main/java/com/example/studentslife/ItemPassive.kt
+++
@@ -1,3 +0,0 @@
-package com.example.studentslife
-
-class ItemPassive (val nameItem: String, val upgrade: Int, val id: String, var cost: Int)
